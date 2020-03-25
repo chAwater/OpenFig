d08bb155c122dbde910c83cc15fe5f6e6efa42c8
chenhe <38067903+chAwater@users.noreply.github.com>
2020-03-25 23:08:35 +0800

    [DEV] Code for download data

---
+++ 2019-nCoV/data_download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+###############################################################################
+# 
+# Author: chenhe
+# Created Time: 2020.03.25 19:08:16
+# 
+###############################################################################
+
+wget -c https://raw.githubusercontent.com/BlankerL/DXY-COVID-19-Data/master/csv/DXYArea.csv
+wget -c https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
+wget -c https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv
+wget -c https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv
+
